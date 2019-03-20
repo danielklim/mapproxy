1593d3286edaba4ba8b2d9406ef377f94481f4fc
Daniel Lim <daniel.k.lim@gmail.com>
2019-03-20 15:35:33 -0700

    incorporate tilematrixset data

--- mapproxy/util/ext/wmtsparse/parse.py
+++ mapproxy/util/ext/wmtsparse/parse.py
@@ -6,11 +6,14 @@ from mapproxy.request.wms import switch_bbox_epsg_axis_order
 
 class WMTSCapabilities(object):
     version = '1.0.0'
+    # order of preference for sources with multiple SRSs
+    crs_priority = ['EPSG:4326', 'EPSG:3857', 'EPSG:900913']
 
     def __init__(self, tree):
         self.tree = tree
         self._layer_tree = None
         self.namespaces = tree.getroot().nsmap
+        self.tile_matrix_sets = self.parse_tilematrixsets()
 
     def findtext(self, tree, xpath):
         x = tree.find(xpath, self.namespaces)
@@ -77,7 +80,6 @@ class WMTSCapabilities(object):
     def layers(self):
         if not self._layer_tree:
             root_layer = self.find(self.tree, '//Contents')
-            # print('l', root_layer, self.tree)
             self._layer_tree = self.parse_layer(root_layer, None)
 
         return self._layer_tree
@@ -88,8 +90,8 @@ class WMTSCapabilities(object):
         # resource = self.find(requests_elem, 'GetMap/DCPType/HTTP/Get/OnlineResource')
         if resource != None:
             resources['format'] = self.attrib(resource, 'format')
-            resources['resourceType'] = self.attrib(resource, 'resourceType')
-            resources['template'] = self.attrib(resource, 'template')
+            resources['type'] = self.attrib(resource, 'resourceType')
+            resources['template_raw'] = self.attrib(resource, 'template')
         return resources
 
     def parse_layer(self, layer_elem, parent_layer):
@@ -106,6 +108,7 @@ class WMTSCapabilities(object):
         return child_layers
 
     def parse_layer_data(self, elem, parent_layer):
+        # TODO: error checking and minimum req info
         layer = dict(
             title=self.findtext(elem, 'ows:Title'),
             abstract=self.findtext(elem, 'ows:Abstract'),
@@ -118,12 +121,62 @@ class WMTSCapabilities(object):
         layer['bbox_srs'] = self.layer_bbox_srs(elem)#, parent_layer)
         layer['url'] = self.requests(elem)
         layer['style'] = self.layer_style(elem)
-        layer['tmsl'] = self.layer_tmsl(elem)
-        # TODO: enhance this with tilematrix data
-        layer['srs'] = ['EPSG:4326']
 
+        tms_priority = self.calc_tms_priority(elem)
+        layer['url']['template'] = self.format_url(layer['url']['template_raw'], 
+            tms=tms_priority, 
+            style=layer['style']
+        )
+
+        layer['srs'] = [tms_priority['crs']]
+
+        # print(layer)
         return layer
 
+    @staticmethod
+    def format_url(url, **kwargs):
+
+        replacements = {
+            '{TileMatrixSet}': lambda d: d['tms']['name'],
+            '{Style}': lambda d: d['style']['name'],
+            '{TileMatrix}': '%(z)s',
+            '{TileRow}': '%(y)s',
+            '{TileCol}': '%(x)s',
+            # Other replacements go here
+        }
+
+        for k,v in replacements.items():
+            if callable(v):
+                url = url.replace(k, str(v(kwargs)))
+            else:
+                url = url.replace(k, str(v))
+
+        return url
+
+    def calc_tms_priority(self, elem):
+        crs_priority_idx = None
+        tms_priority = None
+        tms = None
+        for tmsl in self.layer_tmsl(elem):
+            # print(tmsl)
+
+            if tmsl in self.tile_matrix_sets:
+                tms = self.tile_matrix_sets[tmsl]
+
+            if tms is not None and \
+                self.tile_matrix_sets[tmsl]['crs'] in self.crs_priority:
+
+                # print(tms)
+                if crs_priority_idx is None or \
+                    self.crs_priority.index(tms['crs']) < crs_priority_idx:
+                    
+                    crs_priority_idx = self.crs_priority.index(tms['crs'])
+                    tms_priority = tms
+
+        # if none are in priority list, just return last ¯\_(ツ)_/¯
+        return tms_priority if tms_priority is not None else tms
+
+    # return list of `TileMatrixSetLink\TileMatrixSet`s
     def layer_tmsl(self, elem):
         elems = self.findall(elem, 'TileMatrixSetLink')
         x = []
@@ -137,9 +190,9 @@ class WMTSCapabilities(object):
         elem = self.find(elem, 'Style')
         x = {}
         if elem != None:
-            x['isDefault'] = self.attrib(elem, 'isDefault')
-            x['ows:Title'] = self.findtext(elem, 'ows:Title')
-            x['ows:Identifier'] = self.findtext(elem, 'ows:Identifier')
+            x['default'] = self.attrib(elem, 'isDefault')
+            x['title'] = self.findtext(elem, 'ows:Title')
+            x['name'] = self.findtext(elem, 'ows:Identifier')
         return x
 
     def layer_bbox_srs(self, elem):#, parent_layer=None):
@@ -149,16 +202,12 @@ class WMTSCapabilities(object):
             'UNK': 'ows:BoundingBox', 
             'WGS84': 'ows:WGS84BoundingBox',
         }
+
         for srs, w in wrappers.items():
             bbox_srs_elems = self.findall(elem, w)
             if len(bbox_srs_elems) > 0:
                 for bbox_srs_elem in bbox_srs_elems:
-                    crs = self.attrib(bbox_srs_elem, 'crs')
-                    bbox = (
-                        self.findtext(bbox_srs_elem, 'ows:LowerCorner'),
-                        self.findtext(bbox_srs_elem, 'ows:UpperCorner'),
-                    )
-                    bbox = [float(y) for x in bbox for y in x.split(' ')]
+                    crs, bbox = self.parse_bbox(bbox_srs_elem)
                     bbox_srs[w] = {
                         'srs': srs,
                         'crs': crs,
@@ -166,6 +215,43 @@ class WMTSCapabilities(object):
                     }
 
         return bbox_srs
+
+    def parse_bbox(self, elem):
+        crs = self.attrib(elem, 'crs')
+        bbox = (
+            self.findtext(elem, 'ows:LowerCorner'),
+            self.findtext(elem, 'ows:UpperCorner'),
+        )
+        bbox = [float(y) for x in bbox for y in x.split(' ')]
+        return crs, bbox
+
+    @staticmethod
+    def crs2epsgnum(crs):
+        arr = crs.split(':')
+        try:
+            return int(arr[-1])
+        except:
+            return None
+
+    def parse_tilematrixsets(self):
+        tmsl = {}
+        for tms_n in self.findall(self.tree, '/Contents/TileMatrixSet'):
+            tms = {}
+            tms['name'] = self.findtext(tms_n, 'ows:Identifier')
+
+            # no point continuing if no name
+            if tms['name'] is not None:
+                tms['crs_raw'] = self.findtext(tms_n, 'ows:SupportedCRS')
+                tms['crs'] = 'EPSG:{}'.format(self.crs2epsgnum(tms['crs_raw']))
+                tms['scaleset'] = self.findtext(tms_n, 'WellKnownScaleSet')
+                bbox_n = self.find(tms_n, 'ows:BoundingBox')
+                if bbox_n is not None:
+                    crs, tms['bbox'] = self.parse_bbox(bbox_n)
+                else:
+                    crs, tms['bbox'] = None, []
+                tmsl[tms['name']] = tms
+        
+        return tmsl
 
 
 def yaml_sources(cap):
